014ba8fa032be6c45b333ab9405e7f0ff39b9b07
nannabryne <nannabryne@outlook.com>
2023-04-24 16:05:46 +0200

    ikke stort

--- src/plotting/milestone_three.py
+++ src/plotting/milestone_three.py
@@ -52,16 +52,55 @@ k_dict = dict(label     = [k_label(k) for k in k_list],
 
 
 
-PLOT.VelocityPerturbations(df_list, k_dict)
-PLOT.DensityPerturbations(df_list, k_dict)
+# PLOT.VelocityPerturbations(df_list, k_dict)
+# PLOT.DensityPerturbations(df_list, k_dict)
 
-PLOT.PhotonQuadrupole(df_list, k_dict)
-PLOT.GravitationalPotential(df_list, k_dict)
-
-
-# PLOT.SanityChecks(df_list)
+# PLOT.PhotonQuadrupole(df_list, k_dict)
+# PLOT.GravitationalPotential(df_list, k_dict)
 
 
-plt.show()
+# # PLOT.SanityChecks(df_list)
 
+
+# plt.show()
+
+
+
+# data_cosmo = read_ASCII("background_cosmology")
+
+# x_cosmo, eta = data_cosmo[:,0], data_cosmo[:,1]
+
+# fig, ax = plt.subplots()
+
+
+# for k in k_list:
+#     ax.plot(x_cosmo, k/const.Mpc*eta)
+
+# ax.axhline(1,color="k")
+# ax.set_yscale("log")
+# plt.show()
+
+
+
+# Ga2sec =  1e9 * 365*24*60*60
+# sec2Ga = 1/Ga2sec
+
+# ck_inv_list = []
+# for k in k_list:
+#     ck_inv = 1/(k/const.Mpc * const.c) * sec2Ga
+#     ck_inv_list.append(ck_inv)
+
+
+# print(ck_inv_list)
+
+# print(1/(1e-4/const.Mpc * const.c) * sec2Ga)
+# print(1/(1e-5/const.Mpc * const.c) * sec2Ga)
+
+# eta0  = 46.32*const.c * Ga2sec  # eta in sec
+# print(1e-4/const.Mpc*eta0)
+
+
+
+
+# print(1/(eta0/const.Mpc))
 
